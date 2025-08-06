1218514883d791b41f10f0e834815f32abd73654
daniellimotto <daniellimotto10@gmail.com>
2025-08-06 15:10:08 +0800

    initial commit
    springframework

---
+++ Finance-Gudang/src/main/java/GudangFinance/Finance/Gudang/FinanceGudangApplication.java
@@ -0,0 +1,13 @@
+package GudangFinance.Finance.Gudang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceGudangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceGudangApplication.class, args);
+	}
+
+}
---
+++ Finance-Gudang/src/test/java/GudangFinance/Finance/Gudang/FinanceGudangApplicationTests.java
@@ -0,0 +1,13 @@
+package GudangFinance.Finance.Gudang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceGudangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
